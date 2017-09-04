215b9f270850eca293ed8da10bf8a114b977ce47
Tyler Long <tyler4long@gmail.com>
2017-09-04 12:13:50 +0800

    Add coverage reporter

--- karma.conf.js
+++ karma.conf.js
@@ -34,13 +34,22 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'build/dist/dagre-d3.bundle.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+
+    coverageReporter: {
+      dir: 'build/cov',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
 
     // web server port
     port: 9876,
