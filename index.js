fc8a05171dd236e4cee8e96fd6c7e9c6435b8de6
Tyler Long <tyler4long@gmail.com>
2017-09-04 13:16:29 +0800

    Apply JS Standard style

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-module.exports =  {
-  graphlib: require("./lib/graphlib"),
-  dagre: require("./lib/dagre"),
-  intersect: require("./lib/intersect"),
-  render: require("./lib/render"),
-  util: require("./lib/util"),
-  version: require("./lib/version")
-};
+module.exports = {
+  graphlib: require('./lib/graphlib'),
+  dagre: require('./lib/dagre'),
+  intersect: require('./lib/intersect'),
+  render: require('./lib/render'),
+  util: require('./lib/util'),
+  version: require('./lib/version')
+}
