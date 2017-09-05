fcc33bcfa215c083709ae351e69906a5622d0979
Tyler Long <tyler4long@gmail.com>
2017-09-05 12:56:17 +0800

    Refactor code

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-const path = require('path')
-const _ = require('lodash')
-const browserSync = require('browser-sync')
-const changed = require('gulp-changed')
-const del = require('del')
-const fs = require('fs')
-const gulp = require('gulp')
-const karma = require('karma').server
-const replace = require('gulp-replace')
-const shell = require('gulp-shell')
-const watch = require('gulp-watch')
+import path from 'path'
+import _ from 'lodash'
+import browserSync from 'browser-sync'
+import changed from 'gulp-changed'
+import del from 'del'
+import fs from 'fs'
+import gulp from 'gulp'
+import { server } from 'karma'
+import replace from 'gulp-replace'
+import shell from 'gulp-shell'
+import watch from 'gulp-watch'
 
 const BUILD_DIR = 'build'
 const BUILD_DIST_DIR = 'build/dist'
@@ -18,21 +18,21 @@ const DEMO_BUILD = BUILD_DIST_DIR + '/demo'
 
 gulp.task('demo:build', function () {
   return gulp.src(DEMO_SRC)
-        .pipe(replace('../' + BUILD_DIST_DIR + '/dagre-d3.js', '../dagre-d3.js'))
-        .pipe(gulp.dest(DEMO_BUILD))
+    .pipe(replace('../' + BUILD_DIST_DIR + '/dagre-d3.js', '../dagre-d3.js'))
+    .pipe(gulp.dest(DEMO_BUILD))
 })
 
 gulp.task('demo:watch', ['demo:build'], function () {
   gulp.src(DEMO_SRC)
-        .pipe(watch(DEMO_SRC), { verbose: true })
-        .pipe(changed(DEMO_BUILD))
-        .pipe(replace('../' + BUILD_DIST_DIR + '/dagre-d3.js', '../dagre-d3.js'))
-        .pipe(gulp.dest(DEMO_BUILD))
+    .pipe(watch(DEMO_SRC), { verbose: true })
+    .pipe(changed(DEMO_BUILD))
+    .pipe(replace('../' + BUILD_DIST_DIR + '/dagre-d3.js', '../dagre-d3.js'))
+    .pipe(gulp.dest(DEMO_BUILD))
 })
 
 gulp.task('demo:test', ['demo:build'], function () {
   return gulp.src('test/demo-test.js')
-        .pipe(shell('phantomjs <%= (file.path) %>'))
+    .pipe(shell('phantomjs <%= (file.path) %>'))
 })
 
 gulp.task('js:test', [], function (cb) {
@@ -40,7 +40,7 @@ gulp.task('js:test', [], function (cb) {
 })
 
 gulp.task('js:test:watch', [], function (cb) {
-  karma.start({
+  server.start({
     configFile: path.join(__dirname, 'karma.conf.js'),
     singleRun: false,
     browsers: ['PhantomJS']
@@ -72,12 +72,12 @@ gulp.task('serve', ['watch'], function () {
 
 gulp.task('dist', ['version:build', 'build'], function () {
   return gulp.src(BUILD_DIST_DIR + '/**/*')
-        .pipe(gulp.dest(DIST_DIR))
+    .pipe(gulp.dest(DIST_DIR))
 })
 
 gulp.task('release', ['dist'], function () {
   return gulp.src('src/release/release.sh')
-        .pipe(shell('<%= (file.path) %>'))
+    .pipe(shell('<%= (file.path) %>'))
 })
 
 gulp.task('clean', function (cb) {
@@ -96,7 +96,7 @@ function karmaSingleRun (conf, cb) {
     args.browsers = process.env.BROWSERS.split(',')
   }
 
-  karma.start(args, cb)
+  server.start(args, cb)
 }
 
 function generateVersionJs (pkg) {
