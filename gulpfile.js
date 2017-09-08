ef125c947a4592c030cfc2531edecd315165a742
Tyler Long <tyler4long@gmail.com>
2017-09-09 00:42:25 +0800

    Remove gulp

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-import gulp from 'gulp'
-import shell from 'gulp-shell'
-
-gulp.task('demo:test', [], function () {
-  return gulp.src('test/demo-test.js')
-    .pipe(shell('phantomjs <%= (file.path) %>'))
-})
