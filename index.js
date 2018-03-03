98583159b0cc693c27cf66d576dd983f12de4313
Tyler Long <tyler4long@gmail.com>
2018-03-03 18:34:16 +0800

    Don't export d3

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-import * as d3 from 'd3'
-
 import intersect from './lib/intersect'
 import render from './lib/render'
 import util from './lib/util'
 
 export default {
-  d3,
   intersect,
   render,
   util
